ad2da4395bbdbab72101c30be073dbea1dcad035
Damien TANNEAU <damientanneau@MacBook-Air-de-Damien.local>
2024-03-28 23:24:12 +0100

    a

--- src/components/QRCode.jsx
+++ src/components/QRCode.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function QRCode() {
   return (
     <div id="qrcode">
-      <img src="fb-qrcode.png" alt="https://www.facebook.com/utile.landeda/" />
+      <img src="assets/fb-qrcode.png" alt="https://www.facebook.com/utile.landeda/" />
     </div>
   );
 }
